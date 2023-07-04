2c3c0c987ecb567f769319cc0d2b4471b0b3289a
vaibhav <vaibhavsingh05262001@gmail.com>
2023-07-04 11:02:36 +0530

    "loigin backend router  52:47"

--- src/Screen/Login.jsx
+++ src/Screen/Login.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react"
 import React  from 'react'
-
+import { Link } from "react-router-dom"
 function Login() {
-  const [dataname ,setname] = useState('')
  const [dataemail ,setemail] = useState('')
  const [datapassword ,setpassword] = useState('')
- const [datalocation ,setlocation] = useState('')
     const   handlesubmit= async(e)=>{
 
 e.preventDefault() // syntentc event
@@ -14,7 +12,7 @@ const response = await fetch('http://localhost:4000/api/creatuser',{
     headers:{
  'Content-Type':'application/json',
     },
-    body : JSON.stringify({name:dataname,email:dataemail,password:datapassword,location:datalocation})
+    body : JSON.stringify({email:dataemail,password:datapassword})
     }
 );
 const json = await response.json();
@@ -26,7 +24,25 @@ if(!json.success){
     
   return (
     <div>
-      <h1>  log</h1>
+     <div  className='container'>
+    <form onSubmit={handlesubmit}>
+  
+  <div className="form-group">
+    <label htmlFor="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name='email'
+     value={dataemail} onChange={e => setemail(e.target.value)} ></input>
+    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" name='password'  
+    value={datapassword} onChange={e => setpassword(e.target.value)}></input>
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Submit</button>
+  <Link className='m-3 btn btn-danger' to='/signup'>new  user</Link>
+</form>
+</div>
     </div>
   )
 }
